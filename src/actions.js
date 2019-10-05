bb15adc49a56b650bd921fc2038862b4d770e1fa
ialish <eyal.shaar@gmail.com>
2019-10-05 17:05:00 +0300

    Update

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,4 @@
-import {
-	CHANGE_SEARCH_FIELD,
-	REQUEST_ROBOTS_PENDING,
-	REQUEST_ROBOTS_SUCCESS,
-	REQUEST_ROBOTS_FAILED
-} from './actionTypes'
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS } from './actionTypes'
 import { apiCall } from './api/api'
 
 export const setSearchField = (text) => {
@@ -14,14 +9,14 @@ export const setSearchField = (text) => {
 }
 
 export const requestRobots = () => (dispatch) => {
-	dispatch({ type: REQUEST_ROBOTS_PENDING });
+	dispatch({ type: REQUEST_ROBOTS.PENDING });
 	apiCall('https://jsonplaceholder.typicode.com/users')
 		.then((users) => dispatch({
-			type: REQUEST_ROBOTS_SUCCESS,
+			type: REQUEST_ROBOTS.SUCCESS,
 			payload: users
 		}))
 		.catch((error) => dispatch({
-			type: REQUEST_ROBOTS_FAILED,
+			type: REQUEST_ROBOTS.FAILED,
 			payload: error
 		}));
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,4 @@
-import {
-	CHANGE_SEARCH_FIELD,
-	REQUEST_ROBOTS_PENDING,
-	REQUEST_ROBOTS_SUCCESS,
-	REQUEST_ROBOTS_FAILED
-} from './actionTypes'
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS } from './actionTypes'
 
 const initialStateSearch = {
 	searchField: ''
@@ -26,11 +21,11 @@ const initialStateRequest = {
 
 export const requestRobots = (state = initialStateRequest, action) => {
 	switch (action.type) {
-		case REQUEST_ROBOTS_PENDING:
+		case REQUEST_ROBOTS.PENDING:
 			return {...state, ...{ isPending: true }};
-		case REQUEST_ROBOTS_SUCCESS:
+		case REQUEST_ROBOTS.SUCCESS:
 			return {...state, ...{ robots: action.payload, isPending: false }};
-		case REQUEST_ROBOTS_FAILED:
+		case REQUEST_ROBOTS.FAILED:
 			return {...state, ...{ error: action.payload, isPending: false }};
 		default:
 			return state;
